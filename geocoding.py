4944b93feadd64690f4bb2000128ab2941146ca4
Marin Leontenko <marin.leontenko@gmail.com>
2017-09-17 18:06:27 +0200

    dodan odabir datoteke

--- geocoding.py
+++ geocoding.py
@@ -1,6 +1,13 @@
 import geocoder
 
-gc = geocoder.osm('Dore Pfanove 11 ZAgreb')
-print gc.latlng
-print gc.accuracy
-print gc.address
+file = raw_input("Specify source filepath:")
+
+f = open(file, 'r')
+
+for line in f:
+	print line
+
+#gc = geocoder.osm('Dore Pfanove 11 ZAgreb')
+#print gc.latlng
+#print gc.accuracy
+#print gc.address
